8f32ef210c22372d06f02ff751961c726d3f3d4a
gab0626 <kyhk.0626@outlook.jp>
2015-12-28 05:53:09 +0900

    C#のトレーニング
    文字描画

---
+++ 12月28日/Training C#.cs
@@ -0,0 +1,67 @@
+﻿//class sample
+//{
+//    public static void Main()
+//    {
+//        System.Console.WriteLine("Hello, World!");
+//    }
+//}
+
+//class text01
+//{
+//    public static void Main()
+//    {
+//        int a = 10, b = 10, total;
+
+//        total = a + b;
+
+//        System.Console.WriteLine(a + " + " + b + " = " + total);
+//    }
+//}
+
+//class text02
+//{
+//    public static void Main()
+//    {
+//        int a = 10, b = 10;
+
+//        System.Console.WriteLine(a + " + " + b + " = " + (a + b));
+//    }
+//}
+
+//class text03
+//{
+//    public static void Main()
+//    {
+//        System.Console.Write("a");
+//        System.Console.Write("b");
+//        System.Console.Write("c");
+//        System.Console.WriteLine();
+//    }   
+//}
+
+//class text04
+//{
+//    public static void Main()
+//    {
+//        int x = 10;
+//        System.Console.WriteLine("x = {0}", x);
+//        System.Console.WriteLine("x = {0}, xの10倍は{1}です", x, x * 10);
+//        System.Console.WriteLine("{0}は{1}ですが、{2}は{1}ではありません", "猫", "ほ乳類", "トカゲ");
+//    }
+//}
+
+//class text05
+//{
+//    public static void Main()
+//    {
+//        int x = 10, y = 123456789;
+
+//        System.Console.WriteLine("123456789012345678901234567890");
+//        System.Console.WriteLine("{0,10}", "abc");
+//        System.Console.WriteLine("{0,5}", "def");
+//        System.Console.WriteLine("{0,0}", "ghi");
+//        System.Console.WriteLine("{0,10}{1,10}", "あ", "い");
+//        System.Console.WriteLine("{0,-10}{1,-10", "あ", "い");
+//        System.Console.WriteLine("x = {0,5},y = {1,3}", x, y);
+//    }
+//}
